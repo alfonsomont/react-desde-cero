56f7d90274aa4d6215d5981d86ceb4e221c4b686
Alfonso Montalvo <alfonso.montalvo.m@gmail.com>
2020-12-20 17:43:30 -0500

    Ahora cursos ya no es quemado en el documento si no llamado desde un json en un servidor en myjson placeholder se lo llama con axios.get

--- src/components/organisms/CourseGrid.jsx
+++ src/components/organisms/CourseGrid.jsx
@@ -1,38 +1,46 @@
-import React from "react"
+import Axios from "axios"
+import React, { Component } from "react"
 import CourseCard from "./CourseCard"
 
-const Courses=[
-
-    {  "id": 1,
-      "image":"https://edteam-media.s3.amazonaws.com/courses/small/3ec614b5-a9b7-475c-bf8f-8ba643a129eb.png",  
-      "image2":"https://edteam-media.s3.amazonaws.com/courses/small/3ec614b5-a9b7-475c-bf8f-8ba643a129eb.png", 
-      "nombre":"Alfonso Montalvo",
-      "price":40
-    
-    },
-    {   "id": 2,
-      "image":"https://edteam-media.s3.amazonaws.com/courses/big/85d3d7e4-19db-4cff-a4cb-cbead813b6b5.png",  
-      "image2":"https://edteam-media.s3.amazonaws.com/courses/small/3ec614b5-a9b7-475c-bf8f-8ba643a129eb.png", 
-      "nombre":"Luis Montalvo",
-      "price":50
-    
-    },
-    {   "id": 3,
-      "image":"https://edteam-media.s3.amazonaws.com/courses/big/61e5a210-8dab-412e-a6dc-802c070cc18c.jpg",  
-      "image2":"https://edteam-media.s3.amazonaws.com/courses/small/3ec614b5-a9b7-475c-bf8f-8ba643a129eb.png", 
-      "nombre":"Alfonso",
-      "price":60
-    
-    } ]
 
 
-const CourseGrid = () => (
-<div className="ed-grid m-grid-3">
+class CourseGrid extends Component{
+
+  constructor(props){
+    super(props)
+  this.state={
+
+    cursos:[]
+  }}
+componentDidMount(){
+  Axios.get('http://my-json-server.typicode.com/alfonsomont/json-db/cursos')
+  .then(resp=>this.setState({
+    cursos:resp.data
+  }))
+}
+
+  render(){
+    const {cursos}=this.state
+    return(
+      <div className="ed-grid m-grid-3">
 {
-    Courses.map(c=><CourseCard key={c.id} id={c.id} image={c.image} image2={c.image2} price={c.price}/>)
+    cursos.map(c=><CourseCard key={c.id} id={c.id} image={c.image} image2={c.image2} price={c.price}/>)
 }
 
 </div>
-)
+
+
+    )
+  }
+
+}
+
+
+
+
+
+
+
+
 
 export default CourseGrid
