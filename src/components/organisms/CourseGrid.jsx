bdda06c22e5eb2cf20a7ddd42a152dac39f6186e
Alfonso Montalvo <alfonso.montalvo.m@gmail.com>
2020-12-20 19:29:12 -0500

    Se agrego un loader o mensaje de Cargando... a cursos y users

--- src/components/organisms/CourseGrid.jsx
+++ src/components/organisms/CourseGrid.jsx
@@ -6,32 +6,39 @@ import CourseCard from "./CourseCard"
 
 class CourseGrid extends Component{
 
-  constructor(props){
+    constructor(props){
     super(props)
-  this.state={
+    this.state={
 
     cursos:[]
-  }}
-componentDidMount(){
-  Axios.get('http://my-json-server.typicode.com/alfonsomont/json-db/cursos')
-  .then(resp=>this.setState({
+    }}
+
+
+    componentDidMount(){
+    Axios.get('http://my-json-server.typicode.com/alfonsomont/json-db/cursos')
+    .then(resp=>this.setState({
     cursos:resp.data
-  }))
-}
+    }))
+                      }
 
   render(){
     const {cursos}=this.state
     return(
       <div className="ed-grid m-grid-3">
-{
-    cursos.map(c=><CourseCard key={c.id} id={c.id} image={c.image} image2={c.image2} price={c.price}/>)
-}
+         {cursos.length===0
+         ? <h1 className="t3">Cargando...</h1>
+        : cursos.map(c=><CourseCard
+          key={c.id} 
+           id={c.id} 
+           image={c.image} 
+           image2={c.image2} 
+           price={c.price}/>)
+     }
+        </div>
+            )
+          }
 
-</div>
 
-
-    )
-  }
 
 }
 
--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -12,12 +12,6 @@ class Users extends Component{
         }
     }
 componentDidMount(){
-
-    // fetch('https://jsonplaceholder.typicode.com/users',{method:'get'})
-    // .then(respuesta=>respuesta.json())
-    // .then(respuesta2=>this.setState({users:respuesta2}))
-
-
     axios.get('https://jsonplaceholder.typicode.com/users')
     .then(resp=>{
         
@@ -31,14 +25,16 @@ componentDidMount(){
 
         const {users}=this.state
         return(
-            <div className="ed-grid">
-                <h1>
+                <div className="ed-grid">
+                    <h1>
                     Pagina Usuarios
-                </h1>
-                <div className="ed-grid s-grid-2 m-grid-3 lg-grid-4">
-                {users.map(u=>(<UserCard key={u.id} name={u.name} email={u.email}/>))}
-
-                </div>
+                    </h1>
+                    {users.length===0?
+                    <h1>Cargando...</h1>
+                    :<div className="ed-grid s-grid-2 m-grid-3 lg-grid-4">
+                    {users.map(u=>(<UserCard key={u.id} name={u.name} email={u.email}/>))}
+                    </div>}
+                
                 </div>
         )
 
